bf0f664772b5297334a1cc979918966f106525b3
minkatha <minkatha01@gmail.com>
2016-08-10 22:53:01 -0500

    Add mergesort

--- Algorithms/MergeSort/MergeSort/Program.cs
+++ Algorithms/MergeSort/MergeSort/Program.cs
@@ -1,19 +1,74 @@
-﻿namespace MergeSort
+﻿using System;
+
+namespace MergeSort
 {
-    class Program
+    public class Merge
     {
-        public static void Merge(int[] array, int lo, int mid, int hi)
+        private static int[] aux;
+
+        public static void Sort(int[] a)
+        {
+            aux = new int[a.Length];
+            Sort(a, 0, a.Length - 1);
+        }
+
+        private static void Sort(int[] a, int lo, int hi)
+        {
+            if (hi <= lo)
+            {
+                return;
+            }
+            int mid = lo + (hi - lo) / 2;
+            Sort(a, lo, mid);
+            Sort(a, mid + 1, hi);
+            merge(a, lo, mid, hi);
+        }
+
+        public static void merge(int[] a, int lo, int mid, int hi)
         {
             int i = lo;
             int j = mid + 1;
 
             for (int k = lo; k <= hi; k++)
             {
+                aux[k] = a[k];
+            }
 
+            for (int k = lo; k <= hi; k++)
+            {
+                if (i > mid)
+                {
+                    a[k] = aux[j++];
+                }
+                else if (j > hi)
+                {
+                    a[k] = aux[i++];
+                }
+                else if (aux[j] < aux[i])
+                {
+                    a[k] = aux[j++];
+                }
+                else
+                {
+                    a[k] = aux[i++];
+                }
             }
         }
+
+        public static void PrintArray(int[] array)
+        {
+            foreach (int number in array)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadLine();
+        }
+
         static void Main(string[] args)
         {
+            int[] array = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            Sort(array);
+            PrintArray(array);
         }
     }
 }
